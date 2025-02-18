7b679fd02878f9b74052c49a596656c3adcbb335
DjoserKhemSimeu <djoser.simeu@gmail.com>
2025-02-18 17:08:19 +0100

    work on g5k hardware

---
+++ plot_g5k.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from adjustText import adjust_text
+
+# Charger les données
+data = pd.read_csv('g5k_edge.csv', delimiter=';')
+
+names = data['names']
+die_area = data['Die_area']
+tdp = data['TDP']
+gflops = data['GFLOPS']
+price = data['price']
+
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(20, 7))
+
+norm = plt.Normalize(vmin=price.min(), vmax=price.max())
+cmap = plt.cm.viridis
+
+
+sc1 = axes[0].scatter(tdp, die_area, c=price, cmap=cmap, norm=norm)
+
+
+coeffs1 = np.polyfit(tdp, die_area, 2)
+poly1 = np.poly1d(coeffs1)
+x_range = np.linspace(min(tdp), max(tdp), 100)
+axes[0].plot(x_range, poly1(x_range), color='red', linestyle='--', label="Quadratic regression")
+
+texts = []
+for i, name in enumerate(names):
+    texts.append(axes[0].annotate(name, (tdp[i], die_area[i]), fontsize=12, ha='right',
+                                  arrowprops=dict(arrowstyle="->", lw=0.5, color='gray')))
+adjust_text(texts, only_move={'points':'xy', 'text':'xy'}, max_move=(10,10), ax=axes[0])
+axes[0].set_xlabel('TDP')
+axes[0].set_ylabel('Die Area')
+axes[0].set_title('TDP vs Die Area')
+axes[0].legend()
+
+
+sc2 = axes[1].scatter(tdp, gflops, c=price, cmap=cmap, norm=norm)
+
+
+coeffs2 = np.polyfit(tdp, gflops, 2)
+poly2 = np.poly1d(coeffs2)
+axes[1].plot(x_range, poly2(x_range), color='red', linestyle='--', label="Quadratic regression")
+
+texts = []
+for i, name in enumerate(names):
+    texts.append(axes[1].annotate(name, (tdp[i], gflops[i]), fontsize=12, ha='right',
+                                  arrowprops=dict(arrowstyle="->", lw=0.5, color='gray')))
+adjust_text(texts, only_move={'points':'xy', 'text':'xy'}, max_move=(10,10), ax=axes[1])
+axes[1].set_xlabel('TDP')
+axes[1].set_ylabel('GFLOPS')
+axes[1].set_title('TDP vs GFLOPS')
+axes[1].legend()
+
+
+cbar = fig.colorbar(sc1, ax=axes, orientation='vertical', fraction=0.015, pad=0.04)
+cbar.set_label('Price')
+
+plt.show()
--- roofline_model.py
+++ roofline_model.py
@@ -4,7 +4,7 @@ import numpy as np
 
 bandwidth = 1.56e12  # en bytes par seconde
 peak_performance = 19.49e12  # en FLOPS
-computational_intensity = 1143.055074
+computational_intensity = 285.764
 
 
 operational_intensity = np.linspace(0.01, 10**5, 400)
@@ -13,12 +13,12 @@ operational_intensity = np.linspace(0.01, 10**5, 400)
 bandwidth_limited_performance = bandwidth * operational_intensity
 
 bandwidth_jetson = 136.5e9  # en bytes par seconde
-peak_performance_jetson = 1.41e9  # en FLOPS
+peak_performance_jetson = 1.41e12  # en FLOPS
 
 bandwidth_limited_performance_jetson = bandwidth_jetson * operational_intensity
 
 
-fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(30, 14))
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(20, 11))
 
 # Nvidia A100
 axes[0].plot(operational_intensity, bandwidth_limited_performance, label='Bandwith limitation', color='blue')
@@ -29,6 +29,8 @@ axes[0].set_yscale('log')
 axes[0].set_xlabel('Operational Intensity (FLOPs/byte)')
 axes[0].set_ylabel('Performance (FLOPS)')
 axes[0].set_title('Roofline model - Nvidia A100 SXM4 - 40G')
+axes[0].fill_betweenx([0, min(peak_performance, bandwidth * computational_intensity)],
+                      computational_intensity, max(operational_intensity), color='green', alpha=0.3, hatch='/',label="StarCoder region")
 axes[0].legend()
 axes[0].grid(True)
 
@@ -46,13 +48,25 @@ axes[1].set_xscale('log')
 axes[1].set_yscale('log')
 axes[1].set_xlabel('Operational Intensity (FLOPs/byte)')
 axes[1].set_ylabel('Performance (FLOPS)')
-axes[1].set_title('Roofline model - Nvidia Jetson AMX Xavier')
+axes[1].set_title('Roofline model - Nvidia Jetson AMX Xavier - 32G')
+axes[1].fill_betweenx([0, min(peak_performance_jetson, bandwidth_jetson * computational_intensity)],
+                      computational_intensity, max(operational_intensity), color='green', alpha=0.3, hatch='/',label="StarCoder region")
 axes[1].legend()
 axes[1].grid(True)
+
 axes[1].annotate(f'{min(peak_performance_jetson, bandwidth_jetson * computational_intensity):.2e} FLOPS',
              xy=(computational_intensity, min(peak_performance_jetson, bandwidth_jetson * computational_intensity)),
              xytext=(1e3, 1e10),
              arrowprops=dict(facecolor='black', arrowstyle='->'),
              fontsize=18)
+x_min = min(operational_intensity)
+x_max = max(operational_intensity)
+y_min = min(min(bandwidth_limited_performance), min(bandwidth_limited_performance_jetson))
+y_max = max(max(bandwidth_limited_performance), max(bandwidth_limited_performance_jetson), peak_performance, peak_performance_jetson)
+
+axes[0].set_xlim([x_min, x_max])
+axes[0].set_ylim([y_min, y_max])
+axes[1].set_xlim([x_min, x_max])
+axes[1].set_ylim([y_min, y_max])
 plt.tight_layout()
 plt.show()
