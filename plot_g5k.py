21e70985705eff65415e7049251c3b1fd954da81
DjoserKhemSimeu <djoser.simeu@gmail.com>
2025-02-19 16:04:26 +0100

    updates on TGI

--- plot_g5k.py
+++ plot_g5k.py
@@ -11,9 +11,9 @@ die_area = data['Die_area']
 tdp = data['TDP']
 gflops = data['GFLOPS']
 price = data['price']
-
+env = (((tdp-tdp.min())/(tdp.max()-tdp.min()))+((die_area-die_area.min())/(die_area.max()-die_area.min())))/2
 fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(20, 7))
-
+print(env)
 norm = plt.Normalize(vmin=price.min(), vmax=price.max())
 cmap = plt.cm.viridis
 
@@ -37,21 +37,22 @@ axes[0].set_title('TDP vs Die Area')
 axes[0].legend()
 
 
-sc2 = axes[1].scatter(tdp, gflops, c=price, cmap=cmap, norm=norm)
+sc2 = axes[1].scatter(env, gflops, c=price, cmap=cmap, norm=norm)
 
 
-coeffs2 = np.polyfit(tdp, gflops, 2)
+coeffs2 = np.polyfit(env, gflops, 2)
 poly2 = np.poly1d(coeffs2)
+x_range = np.linspace(min(env), max(env), 100)
 axes[1].plot(x_range, poly2(x_range), color='red', linestyle='--', label="Quadratic regression")
 
 texts = []
 for i, name in enumerate(names):
-    texts.append(axes[1].annotate(name, (tdp[i], gflops[i]), fontsize=12, ha='right',
+    texts.append(axes[1].annotate(name, (env[i], gflops[i]), fontsize=12, ha='right',
                                   arrowprops=dict(arrowstyle="->", lw=0.5, color='gray')))
 adjust_text(texts, only_move={'points':'xy', 'text':'xy'}, max_move=(10,10), ax=axes[1])
-axes[1].set_xlabel('TDP')
-axes[1].set_ylabel('GFLOPS')
-axes[1].set_title('TDP vs GFLOPS')
+axes[1].set_xlabel('Edge level ((TDP_norm+Die_norm)/2)')
+axes[1].set_ylabel('GFLOPS(FP32)')
+axes[1].set_title('Edge level vs GFLOPS')
 axes[1].legend()
 
 
