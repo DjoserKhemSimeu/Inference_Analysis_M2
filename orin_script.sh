5188fecdd69b33393c3042a8dbb93a1b6105c5a4
DjoserKhemSimeu <djoser.simeu@gmail.com>
2025-05-21 11:02:13 +0200

    adding orin script

---
+++ orin_script.sh
@@ -0,0 +1,21 @@
+sudo apt update
+sudo apt install python3-pip
+sudo pip3 install -U jetson-stats
+sudo apt install nvidia-jetpack
+#cd /tmp
+git clone https://github.com/DjoserKhemSimeu/inference_results_v4.1.git
+mkdir scratch
+export MLPERF_SCRATCH_PATH=/tmp/scratch/
+mkdir $MLPERF_SCRATCH_PATH/data $MLPERF_SCRATCH_PATH/models $MLPERF_SCRATCH_PATH/preprocessed_data
+cd inference_results_v4.1/closed/NVIDIA/
+make prebuild
+echo $MLPERF_SCRATCH_PATH
+ls -al $MLPERF_SCRATCH_PATH
+make clean
+make link_dirs
+ls -al build/
+make download_data BENCHMARKS="bert"
+make download_model BENCHMARKS="bert"
+make preprocess_data BENCHMARKS="bert"
+make build
+make run RUN_ARGS="--benchmarks=bert --scenarios=singlestream"
